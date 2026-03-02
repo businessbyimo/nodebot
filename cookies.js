fa2b2e973b8d36ee6bb18749f2118daa85d8a133
businessbyimo <businessbyimo@gmail.com>
2026-03-02 21:36:44 +0600

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,91 +1,41 @@
-// cookies.js
-// ফেসবুক কুকি - সম্পূর্ণ ঠিক করা
-
+// cookies.js - সুপার ফাইনাল ফিক্স
 module.exports = [
   {
-    "domain": "facebook.com",
-    "hostOnly": true,
+    "domain": ".facebook.com",
+    "hostOnly": false,
     "httpOnly": true,
     "name": "datr",
     "path": "/",
-    "sameSite": "no_restriction",
     "secure": true,
     "session": false,
     "value": "jj-jaV2xHNKXBpof3DPdx2W2"
   },
   {
-    "domain": "facebook.com",
-    "hostOnly": true,
+    "domain": ".facebook.com",
+    "hostOnly": false,
     "httpOnly": true,
     "name": "sb",
     "path": "/",
-    "sameSite": "no_restriction",
     "secure": true,
     "session": false,
     "value": "mD-jadYEgqeCvOOWd8Q3SOhs"
   },
   {
-    "domain": "facebook.com",
-    "hostOnly": true,
-    "httpOnly": true,
-    "name": "ps_l",
-    "path": "/",
-    "sameSite": "lax",
-    "secure": true,
-    "session": false,
-    "value": "1"
-  },
-  {
-    "domain": "facebook.com",
-    "hostOnly": true,
-    "httpOnly": true,
-    "name": "ps_n",
-    "path": "/",
-    "sameSite": "no_restriction",
-    "secure": true,
-    "session": false,
-    "value": "1"
-  },
-  {
-    "domain": "facebook.com",
-    "hostOnly": true,
+    "domain": ".facebook.com",
+    "hostOnly": false,
     "httpOnly": false,
     "name": "c_user",
     "path": "/",
-    "sameSite": "no_restriction",
     "secure": true,
     "session": false,
     "value": "61575121907959"
   },
   {
-    "domain": "facebook.com",
-    "hostOnly": true,
-    "httpOnly": false,
-    "name": "wd",
-    "path": "/",
-    "sameSite": "lax",
-    "secure": true,
-    "session": false,
-    "value": "1280x935"
-  },
-  {
-    "domain": "facebook.com",
-    "hostOnly": true,
-    "httpOnly": true,
-    "name": "fr",
-    "path": "/",
-    "sameSite": "no_restriction",
-    "secure": true,
-    "session": false,
-    "value": "1FXmuOrU32oKUfE1n.AWdd-58ndz2x5MEMdou7HuBxA3HNgCA9eCV5IzoZPiJrsPl4enk.BppaT3..AAA.0.0.BppaT3.AWdiGPzqWUoM6qhX6mCIsiywWPo"
-  },
-  {
-    "domain": "facebook.com",
-    "hostOnly": true,
+    "domain": ".facebook.com",
+    "hostOnly": false,
     "httpOnly": true,
     "name": "xs",
     "path": "/",
-    "sameSite": "no_restriction",
     "secure": true,
     "session": false,
     "value": "19%3ANox1zl1Dx4ThZA%3A2%3A1772390714%3A-1%3A-1%3A%3AAcxf4hWX9HPRt1WSajUkJJeQ_p_1GZCPgwqs4-vS8g"
